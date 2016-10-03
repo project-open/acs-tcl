e79865582d732760ae27fde96b09268be0a2fcc0
Frank Bergmann <frank.bergmann@project-open.com>
2016-10-03 17:57:25 +0000

    - debugging messages

--- tcl/json-procs.tcl
+++ tcl/json-procs.tcl
@@ -102,11 +102,13 @@ ad_proc util::json::parse {jsonText} {
     @return List containing the object represented by jsonText
 
 } {
+    # ns_log Notice "util::json::parse: jsonText=$jsonText"
     variable tokenRE
 
     set tokens [regexp -all -inline -- $tokenRE $jsonText]
     set nrTokens [llength $tokens]
     set tokenCursor 0
+
     return [parseValue $tokens $nrTokens tokenCursor]
 }
 
@@ -214,6 +216,7 @@ ad_proc -private util::json::parseObject {tokens nrTokens tokenCursorName} {
     @return parsed object (Tcl dict)
 } {
     upvar $tokenCursorName tokenCursor
+    # ns_log Notice "util::json::parseObject: tokens=$tokens, nrTokens=$nrTokens, tokenCursorName=$tokenCursorName, tokenCursor=$tokenCursor"
 
     if {$tokenCursor == $nrTokens} {
         unexpected $tokenCursor "END" "OBJECT"
@@ -313,6 +316,7 @@ ad_proc -private util::json::parseValue {tokens nrTokens tokenCursorName} {
     @return parsed value (dict, list, string, number)
 } {
     upvar $tokenCursorName tokenCursor
+    # ns_log Notice "util::json::parseValue: tokens=$tokens, nrTokens=$nrTokens, tokenCursorName=$tokenCursorName, tokenCursor=$tokenCursor"
 
     if {$tokenCursor == $nrTokens} {
         unexpected $tokenCursor "END" "VALUE"
